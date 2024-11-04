0ca59b9d436c38055af462040fa5967ce314dfae
rohanshevate <85760038+rohanshevate@users.noreply.github.com>
2024-11-04 20:53:08 +0530

    Update models.py

--- reportproject/reportcard/models.py
+++ reportproject/reportcard/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class SubjectMarks(models.model):
+class SubjectMarks(models.Model):
     student = models.Foreignkey(Student, related_name="studentmarks", on_delete=models.CASCADE)
     subject = models.Foreignkey(Subject, on_delete=models.CASCADE)
     marks = models.IntegerField()
