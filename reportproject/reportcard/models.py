7a215b0cefa2cc091ff958b2ec60e26276751b3e
rohanshevate <85760038+rohanshevate@users.noreply.github.com>
2024-11-04 15:15:12 +0000

    card app created

---
+++ reportproject/reportcard/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class SubjectMarks(models.model):
+    student = models.Foreignkey(Student, related_name="studentmarks", on_delete=models.CASCADE)
+    subject = models.Foreignkey(Subject, on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    class Meta:
+        unique_together = ["student", "subject"]
